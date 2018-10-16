e9778f8f82c68259cca6912673f186008c27d3bf
zecollokaris <collo.kariss@gmail.com>
2018-10-16 14:58:21 +0300

    added test for name

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -6,7 +6,16 @@ public class PersonTesst {
 
     @Test
     public void person_instantiatesCorretly_true(){
-        Person testPerson = new Person("Henry",henry@vitualpets.com);
+        Person testPerson = new Person("Henry","henry@vitualpets.com");
         assertEquals(true, testPerson instanceof Person);
     }
+
+    @Test
+    public void getName_personInstantiatesWithName_Henry(){
+        Person testPerson = new Person("Henry","henry@vitualpets.com")
+        assertEquals("Henry", testPerson.getName());
+    }
+
+
+
 }
